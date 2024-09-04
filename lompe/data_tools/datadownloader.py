cd45af4ca4d9aa3cbac6c0d1733647175de06615
Fasil Kebede <46403603+FasilGibdaw@users.noreply.github.com>
2024-09-04 22:47:21 +0200

    updated downloader cdaweb separate doenload ssusi

--- lompe/data_tools/datadownloader.py
+++ lompe/data_tools/datadownloader.py
@@ -16,10 +16,175 @@ warnings.filterwarnings("ignore")
 # the functions are designed to be used in the lompe package for data loading and processing
 # this has to be integrated with the dataloader.py in the lompe package at some level to get lompe data format
 
-# the idea is to merge the two files (datadownloader.py and dataloader.py) into one file in the lompe package, for the time being we need to keep 
+# the idea is to merge the two files (datadownloader.py and dataloader.py) into one file in the lompe package, for the time being we need to keep
 # both, some functions imported from dataloader.py are in use in this script
 
 # Note that: lazy importing is implemented here
+
+
+def download_ssusi_cdaweb(event, hemi='north', basepath='./ssusi_tempfiles', tempfile_path='./', source='cdaweb'):
+    """
+
+    Called and used for modelling auroral conductance in cmodel.py.
+
+    Extract the relevant SSUSI info from netcdf-files downloaded from APL server.
+    https://ssusi.jhuapl.edu/data_products (EDR AURORA)
+    Will load all SSUSI data from specified hemisphere into xarray object. 
+    Any DMSP Block 5D satellite (F16-19) available in folder will be used.
+
+    Parameters
+    ----------
+    event : str
+        string on format 'yyyy-mm-dd' to specify time of event for model.
+    hemi : str, optional
+        specify hemisphere 
+        Default: 'north'
+    basepath : str, optional
+        location of netcdf-files downloaded from APL server (.NC).
+        Default: './ssusi_tempfiles'
+    tempfile_path : str, optional
+        Location for storing processed SSUSI data.
+        Default: './'
+    source : str, optional
+        Specify source of SUSSI data. 
+        Default: 'jhuapl', other option is 'cdaweb'
+
+    Returns
+    -------
+    savefile : str
+        Path to saved file containing SSUSI data + conductances extracted from the images.
+
+    """
+    '''
+    Extract the relevant info from netcdf-files downloaded from APL server:
+    Note that these data are gridded on a 363,363 geomagnetic grid. We assume this is AACGM,
+    based on email correspondence with L. Paxton. This is not clear from the documentation.
+    No geographic information of these gridded data exist in the EDR aurora files downloaded
+    Quote from documentation: "This array is a uniform grid in a polar azimuthal equidistant
+    projection of the geomagnetic latitude and geomagnetic local time, with appropriate
+    geomagnetic pole as the origin; that is: 90-ABS(glat) is the radius and geomagnetic
+    local time is the angle of a two-dimensional polar coordinate system."
+    Hence, the mapped latitude array (363x363) is the same for both hemispheres, and
+    the values are therefore always positive.
+    '''
+    if not basepath.endswith('/'):
+        basepath += '/'
+    if not tempfile_path.endswith('/'):
+        tempfile_path += '/'
+
+    # check if the processed file exists
+    savefile = tempfile_path + \
+        event.replace('-', '') + '_ssusi_' + hemi + '.nc'
+    if os.path.isfile(savefile):
+        print('SSUSI file already exists.')
+        return savefile
+    try:
+        import netCDF4
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError(
+            'read_ssusi: Could not load netCDF4 module. Will not be able to read SSUSI-files from APL.')
+    # downlaoding the files from the server see the ssusi_direct.py for the implementation
+    # from lompe.data_tools.ssusi_direct2 import download_ssusi_files
+    download_ssusi_files(event, basepath=basepath, source=source)
+    ###
+    imgs = []  # List to hold all images. Converted to xarray later.
+    doy_str = f"{date2doy(event):03d}"
+    for sat in ['F16', 'F17', 'F18', 'F19']:
+        files = glob.glob(basepath + '*' + sat.lower() + '*' +
+                          event[0:4] + doy_str + '*.nc')
+        files.sort()
+        if len(files) == 0:
+            continue
+
+        ii = 0  # counter
+
+        for file in files:
+            f = netCDF4.Dataset(file)
+
+            mlat = f.variables['LATITUDE_GEOMAGNETIC_GRID_MAP'][:]
+            mlon = f.variables['LONGITUDE_GEOMAGNETIC_' +
+                               hemi.upper() + '_GRID_MAP'][:]
+            mlt = f.variables['MLT_GRID_MAP'][:]
+            uthr = f.variables['UT_' + hemi.upper()[0]][:]
+            doy = int(f.variables['DOY'][:])
+            year = int(f.variables['YEAR'][:])
+
+            wavelengths = f.variables['DISK_RADIANCEDATA_INTENSITY_' + hemi.upper()][:]
+            char_energy = f.variables['ELECTRON_MEAN_' +
+                                      hemi.upper() + '_ENERGY_MAP'][:]
+            energyflux = f.variables['ENERGY_FLUX_' + hemi.upper() + '_MAP'][:]
+
+            f.close()
+
+            mask = uthr == 0
+
+            uthr[mask] = np.nan
+            wavelengths[:, mask] = np.nan
+            char_energy[mask] = np.nan
+            energyflux[mask] = np.nan
+            mlon[mask] = np.nan
+
+            # Applying Robinson formulas to calculate conductances: https://agupubs.onlinelibrary.wiley.com/doi/epdf/10.1029/JA092iA03p02565
+            SP = (40. * char_energy * np.sqrt(energyflux)) / \
+                (16. + char_energy**2)
+            SH = 0.45 * char_energy**0.85 * SP
+
+            if sum(np.isfinite(uthr[:, 181]).flatten()) > 0:  # there is data in north
+                # Calculate center time:
+                # there is a sudden transition across midnight in the pass
+                if np.nanstd(uthr) > 0.5:
+                    next_day = (uthr > 0) & (uthr < 2)
+                    if sum(next_day.flatten()) > 0:
+                        uthr[next_day] = uthr[next_day] + 24.
+                center_hr = np.nanmean(uthr, axis=0)[181]
+                hr = int(center_hr)
+                if hr >= 24:
+                    hr = hr - 24
+                    center_hr = center_hr - 24
+
+                m = int((center_hr - hr) * 60)
+                s = round(((center_hr - hr) * 60 - m) * 60)
+                if s == 60:
+                    t0 = dt.datetime(year, 1, 1, hr, m, 59) + \
+                        dt.timedelta(seconds=1)
+                else:
+                    t0 = dt.datetime(year, 1, 1, hr, m, s)
+
+                dtime = t0 + dt.timedelta(doy - 1)
+                # put into xarray object
+                img = xr.Dataset({'uthr': (['row', 'col'], uthr),
+                                  'mlon': (['row', 'col'], mlon),
+                                  'mlat': (['row', 'col'], mlat),
+                                  'mlt': (['row', 'col'], mlt),
+                                  '1216': (['row', 'col'], wavelengths[0, :, :]),
+                                  '1304': (['row', 'col'], wavelengths[1, :, :]),
+                                  '1356': (['row', 'col'], wavelengths[2, :, :]),
+                                  'lbhs': (['row', 'col'], wavelengths[3, :, :]),
+                                  'lbhl': (['row', 'col'], wavelengths[4, :, :]),
+                                  'E0': (['row', 'col'], char_energy),
+                                  'je': (['row', 'col'], energyflux),
+                                  'SP': (['row', 'col'], SP),
+                                  'SH': (['row', 'col'], SH)})
+                img = img.expand_dims(date=[dtime])
+                img = img.assign({'satellite': sat})
+                img = img.assign(
+                    {'orbit': file.split('-')[-1].split('_')[0][3:]})
+                imgs.append(img)
+            ii += 1
+
+    if len(imgs) == 0:
+        print('No SSUSI images found.')
+
+    else:     # save as netcdf in specified path
+        imgs = xr.concat(imgs, dim='date')
+        imgs = imgs.assign({'hemisphere': hemi})
+        imgs = imgs.sortby(imgs['date'])
+        print('DMSP SSUSI file saved: ' + savefile)
+
+        imgs.to_netcdf(savefile)
+        shutil.rmtree(basepath)
+        return savefile
+
 
 def download_ssusi(event, hemi='north', basepath='./ssusi_tempfiles', tempfile_path='./', source='jhuapl'):
     """
@@ -82,7 +247,7 @@ def download_ssusi(event, hemi='north', basepath='./ssusi_tempfiles', tempfile_p
     except ModuleNotFoundError:
         raise ModuleNotFoundError(
             'read_ssusi: Could not load netCDF4 module. Will not be able to read SSUSI-files from APL.')
-    ## downlaoding the files from the server see the ssusi_direct.py for the implementation
+    # downlaoding the files from the server see the ssusi_direct.py for the implementation
     # from lompe.data_tools.ssusi_direct2 import download_ssusi_files
     download_ssusi_files(event, basepath=basepath, source=source)
     ###
@@ -90,12 +255,8 @@ def download_ssusi(event, hemi='north', basepath='./ssusi_tempfiles', tempfile_p
     doy_str = f"{date2doy(event):03d}"
 
     for sat in ['F16', 'F17', 'F18', 'F19']:
-        if source == 'jhuapl':
-            files = glob.glob(basepath + '*' + sat + '*' +
-                              event[0:4] + event[5:7] + event[8:10] + '*.NC')
-        elif source == 'cdaweb':
-            files = glob.glob(basepath + '*' + sat + '*' +
-                              event[0:4] + doy_str + '*.nc')
+        files = glob.glob(basepath + '*' + sat + '*' +
+                          event[0:4] + event[5:7] + event[8:10] + '*.NC')
         files.sort()
         if len(files) == 0:
             continue
@@ -208,9 +369,9 @@ def download_ssusi(event, hemi='north', basepath='./ssusi_tempfiles', tempfile_p
         print('DMSP SSUSI file saved: ' + savefile)
 
         imgs.to_netcdf(savefile)
-        
+        shutil.rmtree(basepath)
         return savefile
-    
+
 
 def fetch_ssusi_urls(sat, year, doy, source):
     doy_str = f"{doy:03d}"  # Zero-pad the day of year to three digits
@@ -228,7 +389,7 @@ def fetch_ssusi_urls(sat, year, doy, source):
     except requests.exceptions.RequestException as e:
         print(f"Failed to fetch URL probably no data : \n{e}")
         return []
-    
+
     from urllib.parse import urljoin
     soup = BeautifulSoup(response.content, 'html.parser')
     urls = [urljoin(url, link.get('href')) for link in soup.find_all(
@@ -266,19 +427,20 @@ def download_ssusi_files(event, source='jhuapl', basepath='./ssusi_tempfiles/'):
     doy = date2doy(event)
 
     os.makedirs(basepath, exist_ok=True)
-    
+
     from joblib import Parallel, delayed
     import itertools
     # Use joblib.Parallel to fetch URLs concurrently
     fetch_args = [(sat, year, doy, source) for sat in [16, 17, 18, 19]]
-    results = Parallel(n_jobs=8, backend = 'threading')(delayed(fetch_ssusi_urls)(*args) for args in fetch_args)
+    results = Parallel(n_jobs=8, backend='threading')(
+        delayed(fetch_ssusi_urls)(*args) for args in fetch_args)
     # results = Parallel(n_jobs=-1)(delayed(fetch_ssusi_urls)(*args) for args in fetch_args)
     all_urls = list(itertools.chain.from_iterable(results))
 
     # Use joblib.Parallel to download files concurrently
     download_args = [(url, basepath) for url in all_urls]
-    Parallel(n_jobs=8, backend = 'threading')(delayed(download_ssusi_file)(*args) for args in download_args)
-
+    Parallel(n_jobs=8, backend='threading')(
+        delayed(download_ssusi_file)(*args) for args in download_args)
 
 
 def download_smag(event, tempfile_path='./', hemi='all'):
@@ -393,7 +555,7 @@ def download_smag(event, tempfile_path='./', hemi='all'):
 
 def ampere_parsestart(start):
     # DO NOT EDIT THIS FUNCTION
-    
+
     # internal helper function adapted from supermag_api.py
 
     # takes either list of [yyyy, mo, dd, hh, mm, opt_ss]
@@ -414,7 +576,7 @@ def ampere_parsestart(start):
 
 def ampere_coreurl(page, logon, start, extent):
     # DO NOT EDIT THIS FUNCTION
-    
+
     # internal helper function adapted from supermag_api.py
     baseurl = "https://ampere.jhuapl.edu/"
 
@@ -526,7 +688,8 @@ def download_supermag(event, tempfile_path='./'):
         # remove the tempfiles folder after the hdf file is created
         shutil.rmtree('./tempfiles/')
         return savefile
-    
+
+
 def smag_download_for_station(args, retries=5, backoff_factor=0.5):
     # DO NOT EDIT THIS FUNCTION
     urlstr, station = args
@@ -534,7 +697,7 @@ def smag_download_for_station(args, retries=5, backoff_factor=0.5):
     import certifi
     from requests.exceptions import RequestException
     import time
-    
+
     for i in range(retries):
         try:
             response = requests.get(url, verify=certifi.where())
@@ -547,23 +710,28 @@ def smag_download_for_station(args, retries=5, backoff_factor=0.5):
                     print(f"Received zero bytes for station {station}")
                     raise RequestException("Received zero bytes.")
             else:
-                print(f"Failed to retrieve data for station {station}: {response.status_code}")
-                raise RequestException(f"Bad status code: {response.status_code}")
+                print(
+                    f"Failed to retrieve data for station {station}: {response.status_code}")
+                raise RequestException(
+                    f"Bad status code: {response.status_code}")
         except RequestException as e:
-            print(f"Attempt {i + 1} for station {station} failed with error: {e}")
+            print(
+                f"Attempt {i + 1} for station {station} failed with error: {e}")
             time.sleep(backoff_factor * (2 ** i))
-    
-    print(f"Failed to download data for station {station} after {retries} attempts.")
+
+    print(
+        f"Failed to download data for station {station} after {retries} attempts.")
     return None
+
 
 def smag_data_for_event(start):
     # DONOT EDIT THIS FUNCTION
     os.makedirs('./tempfiles', exist_ok=True)
     duration = 86400  # Duration in seconds (one day)
-    ## lazy importing :) 
+    # lazy importing :)
     from lompe.data_tools.supermag_api import sm_GetUrl, sm_coreurl, sm_keycheck_data
     from joblib import Parallel, delayed
-    
+
     # lompe is already registered in API
     urlstr = sm_coreurl('data-api.php', 'lompe', start, 86400)
     # this is hard coded to 'geo', see supermag_api.py to change it according to your needs
@@ -578,10 +746,9 @@ def smag_data_for_event(start):
 
     # Create a list of arguments to pass to data_download_for_station
     args_list = [(urlstr, station) for station in stations]
-    
-    
-    Parallel(n_jobs=8, backend = 'threading')(delayed(smag_download_for_station)(args) for args in args_list)
 
+    Parallel(n_jobs=8, backend='threading')(
+        delayed(smag_download_for_station)(args) for args in args_list)
 
 
 def download_champ(event, basepath='./', tempfile_path='./'):
@@ -1029,11 +1196,13 @@ def download_dmsp_ssies(event, sat, tempfile_path='./', **madrigal_kwargs):
     os.remove(filename2)
     return savefile
 
+
 def download_eiscat():
-    pass 
+    pass
 # def date2doy(date_str):
 #     date = dt.datetime.strptime(date_str, "%Y-%m-%d")
 #     return date.timetuple().tm_yday
+
 
 if __name__ == '__main__':
     print("This is a module to download data from different sources for a given event date.")
